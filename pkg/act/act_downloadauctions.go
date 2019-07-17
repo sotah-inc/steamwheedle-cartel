f799a9a6d531756049b34e5d075806a6e465c5ba
Adrian Parker <ihsw.aparker@gmail.com>
2019-07-17 11:13:57 -0700

    Fill in expected act download-auctions functionality.

--- pkg/act/act_downloadauctions.go
+++ pkg/act/act_downloadauctions.go
@@ -1,7 +1,75 @@
 package act
 
-import "github.com/sotah-inc/steamwheedle-cartel/pkg/sotah"
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/blizzard"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/sotah"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/util"
+)
 
-func (c Client) DownloadAuctions(realms sotah.RegionRealms) error {
-	return nil
+type DownloadAuctionsInJob struct {
+	RegionName blizzard.RegionName
+	RealmSlug  blizzard.RealmSlug
+}
+
+type DownloadAuctionsOutJob struct {
+	RegionName blizzard.RegionName
+	RealmSlug  blizzard.RealmSlug
+	Data       []byte
+	Err        error
+}
+
+func (job DownloadAuctionsOutJob) ToLogrusFields() logrus.Fields {
+	return logrus.Fields{
+		"error":  job.Err.Error(),
+		"region": job.RegionName,
+		"realm":  job.RealmSlug,
+	}
+}
+
+func (c Client) DownloadAuctions(regionRealms sotah.RegionRealms) chan DownloadAuctionsOutJob {
+	// establishing channels
+	in := make(chan DownloadAuctionsInJob)
+	out := make(chan DownloadAuctionsOutJob)
+
+	// spinning up the workers
+	worker := func() {
+		for inJob := range in {
+			actData, err := c.Call("/", "GET", nil)
+			if err != nil {
+				out <- DownloadAuctionsOutJob{
+					RegionName: inJob.RegionName,
+					RealmSlug:  inJob.RealmSlug,
+					Data:       nil,
+					Err:        err,
+				}
+
+				continue
+			}
+
+			out <- DownloadAuctionsOutJob{
+				RegionName: inJob.RegionName,
+				RealmSlug:  inJob.RealmSlug,
+				Data:       actData.Body,
+				Err:        nil,
+			}
+		}
+	}
+	postWork := func() {
+		close(out)
+	}
+	util.Work(4, worker, postWork)
+
+	// queueing up the regions
+	go func() {
+		for regionName, realms := range regionRealms {
+			for _, realm := range realms {
+				in <- DownloadAuctionsInJob{RegionName: regionName, RealmSlug: realm.Slug}
+			}
+		}
+
+		close(in)
+	}()
+
+	return out
 }
