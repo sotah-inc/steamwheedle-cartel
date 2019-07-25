16ee6dadc8f3b160abbc7c31cbe818600cdf89b7
Adrian Parker <ihsw.aparker@gmail.com>
2019-07-25 12:05:32 -0700

    Fill in expected functionality.

--- pkg/state/run/state_run_cleanupmanifests_run.go
+++ pkg/state/run/state_run_cleanupmanifests_run.go
@@ -1,11 +1,37 @@
 package run
 
 import (
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/blizzard"
 	"github.com/sotah-inc/steamwheedle-cartel/pkg/sotah"
 )
 
 func (sta CleanupManifestsState) Handle(regionRealmTuple sotah.RegionRealmTuple) sotah.Message {
+	realm := sotah.NewSkeletonRealm(
+		blizzard.RegionName(regionRealmTuple.RegionName),
+		blizzard.RealmSlug(regionRealmTuple.RealmSlug),
+	)
+
+	expiredTimestamps, err := sta.manifestBase.GetExpiredTimestamps(realm, sta.manifestBucket)
+	if err != nil {
+		return sotah.NewErrorMessage(err)
+	}
+
+	totalDeleted, err := sta.manifestBase.DeleteAllFromTimestamps(expiredTimestamps, realm, sta.manifestBucket)
+	if err != nil {
+		return sotah.NewErrorMessage(err)
+	}
+
+	res := sotah.CleanupManifestPayloadResponse{
+		RegionRealmTuple: sotah.NewRegionRealmTupleFromRealm(realm),
+		TotalDeleted:     totalDeleted,
+	}
+	data, err := res.EncodeForDelivery()
+	if err != nil {
+		return sotah.NewErrorMessage(err)
+	}
+
 	m := sotah.NewMessage()
+	m.Data = data
 
 	return m
 }
