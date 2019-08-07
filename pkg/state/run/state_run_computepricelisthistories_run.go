8d4f3aa6a36fd64a2c1cd412703332a277968091
Adrian Parker <ihsw.aparker@gmail.com>
2019-08-06 19:10:12 -0700

    Add missing returning of the provided tuple.

--- pkg/state/run/state_run_computepricelisthistories_run.go
+++ pkg/state/run/state_run_computepricelisthistories_run.go
@@ -8,5 +8,13 @@ import (
 func (sta ComputePricelistHistoriesState) Run(tuple sotah.RegionRealmTimestampTuple) sotah.Message {
 	logging.WithField("tuple", tuple).Info("Received tuple")
 
-	return sotah.NewMessage()
+	encodedTuple, err := tuple.EncodeForDelivery()
+	if err != nil {
+		return sotah.NewErrorMessage(err)
+	}
+
+	out := sotah.NewMessage()
+	out.Data = encodedTuple
+
+	return out
 }
