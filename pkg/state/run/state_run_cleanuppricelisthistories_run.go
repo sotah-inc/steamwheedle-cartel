4dd6360e627465427b424cca63159458ede668d6
Adrian Parker <ihsw.aparker@gmail.com>
2019-08-07 20:02:54 -0700

    Fill in intended run func for cleanup-pricelist-histories run state.

---
+++ pkg/state/run/state_run_cleanuppricelisthistories_run.go
@@ -0,0 +1,38 @@
+package run
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/blizzard"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/logging"
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/sotah"
+)
+
+func (sta CleanupPricelistHistoriesState) Run(tuple sotah.RegionRealmTuple) sotah.Message {
+	logging.WithFields(logrus.Fields{"tuple": tuple}).Info("Handling")
+
+	realm := sotah.NewSkeletonRealm(blizzard.RegionName(tuple.RegionName), blizzard.RealmSlug(tuple.RealmSlug))
+	expiredTimestamps, err := sta.pricelistHistoriesBase.GetExpiredTimestamps(realm, sta.pricelistHistoriesBucket)
+	if err != nil {
+		return sotah.NewErrorMessage(err)
+	}
+
+	totalDeleted, err := sta.pricelistHistoriesBase.DeleteAll(realm, expiredTimestamps, sta.pricelistHistoriesBucket)
+	if err != nil {
+		return sotah.NewErrorMessage(err)
+	}
+
+	res := sotah.CleanupPricelistPayloadResponse{
+		RegionName:   tuple.RegionName,
+		RealmSlug:    tuple.RealmSlug,
+		TotalDeleted: totalDeleted,
+	}
+	data, err := res.EncodeForDelivery()
+	if err != nil {
+		return sotah.NewErrorMessage()
+	}
+
+	out := sotah.NewMessage()
+	out.Data = data
+
+	return out
+}
