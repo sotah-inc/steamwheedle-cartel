7951a8497e442889ad93ca40bf3147b2beea07fb
Adrian Parker <ihsw.aparker@gmail.com>
2019-08-11 12:25:19 -0700

    Fill in usage of prune-topics.

--- pkg/state/run/state_run_cleanuppubsubtopics_run.go
+++ pkg/state/run/state_run_cleanuppubsubtopics_run.go
@@ -5,5 +5,13 @@ import (
 )
 
 func (sta CleanupPubsubTopicsState) Run(topicNames []string) sotah.Message {
-	return sotah.NewMessage()
+	encodedResults, err := sta.IO.BusClient.PruneTopics(topicNames).EncodeForDelivery()
+	if err != nil {
+		return sotah.NewErrorMessage(err)
+	}
+
+	out := sotah.NewMessage()
+	out.Data = string(encodedResults)
+
+	return out
 }
