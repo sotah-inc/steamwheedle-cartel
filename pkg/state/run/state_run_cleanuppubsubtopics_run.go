041243c85928313dd04637e39321beee804f1bae
Adrian Parker <ihsw.aparker@gmail.com>
2019-08-11 08:47:50 -0700

    Start run func for cleanup-pubsub-topics state.

---
+++ pkg/state/run/state_run_cleanuppubsubtopics_run.go
@@ -0,0 +1,9 @@
+package run
+
+import (
+	"github.com/sotah-inc/steamwheedle-cartel/pkg/sotah"
+)
+
+func (sta CleanupPubsubTopicsState) Run(topicName []string) sotah.Message {
+	return sotah.NewMessage()
+}
