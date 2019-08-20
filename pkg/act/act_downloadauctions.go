f45eea23f99eeec181ab8b1740b4c30f382a056e
Adrian Parker <ihsw.aparker@gmail.com>
2019-08-20 10:11:02 -0700

    Reduce concurrency because of resource overusage.

--- pkg/act/act_downloadauctions.go
+++ pkg/act/act_downloadauctions.go
@@ -64,7 +64,7 @@ func (c Client) DownloadAuctions(regionRealms sotah.RegionRealms) chan DownloadA
 	postWork := func() {
 		close(out)
 	}
-	util.Work(16, worker, postWork)
+	util.Work(12, worker, postWork)
 
 	// queueing up the regions
 	go func() {
