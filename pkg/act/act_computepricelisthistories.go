d318ef67ed417fbffccfdf4d6e24446ef70b9b60
Adrian Parker <ihsw.aparker@gmail.com>
2019-09-20 10:30:40 -0700

    Increase concurrency.

--- pkg/act/act_computepricelisthistories.go
+++ pkg/act/act_computepricelisthistories.go
@@ -66,7 +66,7 @@ func (c Client) ComputePricelistHistories(
 	postWork := func() {
 		close(out)
 	}
-	util.Work(8, worker, postWork)
+	util.Work(12, worker, postWork)
 
 	// queueing up the regions
 	go func() {
