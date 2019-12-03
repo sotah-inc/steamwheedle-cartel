c7165a6d61bd676fd2a3f138063690e264a91557
Adrian Parker <ihsw.aparker@gmail.com>
2019-12-03 07:44:13 -0700

    Start realm-modification-dates test file.

---
+++ pkg/state/dev/state_api_realmmodificationdates_test.go
@@ -0,0 +1,11 @@
+package dev_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestListenForRealmModificationDates(t *testing.T) {
+	assert.Equal(t, 1, 1, "Testing")
+}
