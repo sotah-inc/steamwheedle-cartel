50a47f82204f1ab25f323de10345b6a9e637645d
Adrian Parker <ihsw.aparker@gmail.com>
2019-12-02 19:24:43 -0700

    Add helpful log message.

--- pkg/state/dev/state_api_realmmodificationdates.go
+++ pkg/state/dev/state_api_realmmodificationdates.go
@@ -2,6 +2,7 @@ package dev
 
 import (
 	nats "github.com/nats-io/go-nats"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/messenger"
 	mCodes "source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/messenger/codes"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/state"
@@ -11,6 +12,8 @@ import (
 func (sta APIState) ListenForRealmModificationDates(stop state.ListenStopChan) error {
 	err := sta.IO.Messenger.Subscribe(string(subjects.RealmModificationDates), stop, func(natsMsg nats.Msg) {
 		m := messenger.NewMessage()
+
+		logging.WithField("realm-modification-dates", sta.RegionRealmModificationDates).Info("Received request")
 
 		encodedData, err := sta.RegionRealmModificationDates.EncodeForDelivery()
 		if err != nil {
