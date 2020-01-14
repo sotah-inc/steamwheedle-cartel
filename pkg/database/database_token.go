2e69ac24c57a830983c962adc690a00bab991bfa
Adrian Parker <ihsw.aparker@gmail.com>
2020-01-14 07:40:36 -0700

    Add region-specific bucketing to the token database.

--- pkg/database/database_token.go
+++ pkg/database/database_token.go
@@ -2,18 +2,20 @@ package database
 
 import (
 	"fmt"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzard"
 	"strconv"
 )
 
 // bucketing
-func databaseTokensBucketName() []byte {
-	return []byte("tokens")
+func databaseTokensBucketName(regionName blizzard.RegionName) []byte {
+	return []byte(fmt.Sprintf("tokens-%s", regionName))
 }
 
 // keying
 func tokenKeyName(lastUpdated int64) []byte {
 	return []byte(fmt.Sprintf("last-updated-%d", lastUpdated))
 }
+
 
 func lastUpdatedFromTokenKeyName(key []byte) (int64, error) {
 	unparsedLastUpdated, err := strconv.Atoi(string(key)[len("last-updated-"):])
