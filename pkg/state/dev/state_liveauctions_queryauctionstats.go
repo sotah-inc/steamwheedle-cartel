d28a1886b06dcd070879586e0c416bf2d87729e4
Adrian Parker <ihsw.aparker@gmail.com>
2020-01-26 10:28:04 -0700

    Start filling in expected functionality.

--- pkg/state/dev/state_liveauctions_queryauctionstats.go
+++ pkg/state/dev/state_liveauctions_queryauctionstats.go
@@ -2,7 +2,9 @@ package dev
 
 import (
 	"github.com/nats-io/go-nats"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/database"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/messenger"
+	mCodes "source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/messenger/codes"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/state"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/state/subjects"
 )
@@ -10,6 +12,47 @@ import (
 func (laState LiveAuctionsState) ListenForQueryRealmModificationDates(stop state.ListenStopChan) error {
 	err := laState.IO.Messenger.Subscribe(string(subjects.QueryAuctionStats), stop, func(natsMsg nats.Msg) {
 		m := messenger.NewMessage()
+
+		req, err := state.NewAuctionsStatsRequest(natsMsg.Data)
+		if err != nil {
+			m.Err = err.Error()
+			m.Code = mCodes.GenericError
+			laState.IO.Messenger.ReplyTo(natsMsg, m)
+
+			return
+		}
+
+		if req.RegionName == "" {
+			totalStats := database.MiniAuctionListGeneralStats{}
+
+			for _, status := range laState.Statuses {
+				for job := range laState.IO.Databases.LiveAuctionsDatabases.GetStats(status.Realms) {
+					if job.Err != nil {
+						m.Err = job.Err.Error()
+						m.Code = mCodes.GenericError
+						laState.IO.Messenger.ReplyTo(natsMsg, m)
+
+						return
+					}
+
+					totalStats = totalStats.Add(job.Stats.MiniAuctionListGeneralStats)
+				}
+			}
+
+			encoded, err := totalStats.EncodeForDelivery()
+			if err != nil {
+				m.Err = err.Error()
+				m.Code = mCodes.GenericError
+				laState.IO.Messenger.ReplyTo(natsMsg, m)
+
+				return
+			}
+
+			m.Data = string(encoded)
+			laState.IO.Messenger.ReplyTo(natsMsg, m)
+
+			return
+		}
 
 		laState.IO.Messenger.ReplyTo(natsMsg, m)
 	})
