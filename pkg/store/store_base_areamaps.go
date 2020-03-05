a0dddaf1f7a41b835ae8fa4d303ebd26a8106e36
Adrian Parker <ihsw.aparker@gmail.com>
2020-03-05 08:05:10 -0700

    Start area-maps store base.

---
+++ pkg/store/store_base_areamaps.go
@@ -0,0 +1,43 @@
+package store
+
+import (
+	"fmt"
+
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/sotah/gameversions"
+
+	"cloud.google.com/go/storage"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/store/regions"
+)
+
+func NewAreaMapsBase(c Client, location regions.Region, gameVersion gameversions.GameVersion) AreaMapsBase {
+	return AreaMapsBase{base{client: c, location: location}, gameVersion}
+}
+
+type AreaMapsBase struct {
+	base
+	GameVersion gameversions.GameVersion
+}
+
+func (b AreaMapsBase) getBucketName() string {
+	return "sotah-areamaps"
+}
+
+func (b AreaMapsBase) GetBucket() *storage.BucketHandle {
+	return b.base.getBucket(b.getBucketName())
+}
+
+func (b AreaMapsBase) GetFirmBucket() (*storage.BucketHandle, error) {
+	return b.base.getFirmBucket(b.getBucketName())
+}
+
+func (b AreaMapsBase) getObjectName(areaId int) string {
+	return fmt.Sprintf("%s/%d.jpg", b.GameVersion, areaId)
+}
+
+func (b AreaMapsBase) GetObject(areaId int, bkt *storage.BucketHandle) *storage.ObjectHandle {
+	return b.base.getObject(b.getObjectName(areaId), bkt)
+}
+
+func (b AreaMapsBase) GetFirmObject(areaId int, bkt *storage.BucketHandle) (*storage.ObjectHandle, error) {
+	return b.base.getFirmObject(b.getObjectName(areaId), bkt)
+}
