b422145f6f4ce601fcf10c79e6051941e0b976a4
Adrian Parker <ihsw.aparker@gmail.com>
2020-03-07 16:29:56 -0700

    Start wowhead package.

---
+++ pkg/wowhead/wowhead.go
@@ -0,0 +1,60 @@
+package wowhead
+
+import (
+	"fmt"
+
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/util"
+)
+
+const AreaMapUrlFormat = "https://wow.zamimg.com/images/wow/classic/maps/enus/original/%d.jpg"
+
+func DownloadAreaMap(id int) ([]byte, error) {
+	return util.Download(fmt.Sprintf(AreaMapUrlFormat, id))
+}
+
+type DownloadAreaMapsJob struct {
+	Err  error
+	Id   int
+	Data []byte
+}
+
+func DownloadAreaMaps(ids []int) chan DownloadAreaMapsJob {
+	// spawning workers
+	in := make(chan int)
+	out := make(chan DownloadAreaMapsJob)
+	worker := func() {
+		for id := range in {
+			data, err := DownloadAreaMap(id)
+			if err != nil {
+				out <- DownloadAreaMapsJob{
+					Err:  err,
+					Id:   id,
+					Data: nil,
+				}
+
+				continue
+			}
+
+			out <- DownloadAreaMapsJob{
+				Err:  nil,
+				Id:   id,
+				Data: data,
+			}
+		}
+	}
+	postWork := func() {
+		close(out)
+	}
+	util.Work(4, worker, postWork)
+
+	// spinning it up
+	go func() {
+		for _, id := range ids {
+			in <- id
+		}
+
+		close(in)
+	}()
+
+	return out
+}
