d6a0724c65026b0f405197956ac4d3b31a07e53c
Adrian Parker <ihsw.aparker@gmail.com>
2020-03-14 16:51:31 -0700

    Start filter-in-valid func.

---
+++ pkg/hell/hell_areamaps_filterinvalid.go
@@ -0,0 +1,7 @@
+package hell
+
+import "source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/sotah"
+
+func (c Client) FilterInValid() (sotah.AreaMapMap, error) {
+	return sotah.AreaMapMap{}, nil
+}
