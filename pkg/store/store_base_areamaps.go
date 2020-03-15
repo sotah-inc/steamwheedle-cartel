6c64bdddde7ded77015550ac91faaf5e1240dcd6
Adrian Parker <ihsw.aparker@gmail.com>
2020-03-15 14:09:21 -0700

    Split into different files.

--- pkg/store/store_base_areamaps.go
+++ pkg/store/store_base_areamaps.go
@@ -5,9 +5,6 @@ import (
 
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/sotah"
 
-	"github.com/sirupsen/logrus"
-	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/util"
-
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/sotah/gameversions"
 
 	"cloud.google.com/go/storage"
@@ -55,54 +52,4 @@ func (b AreaMapsBase) WriteObject(areaId sotah.AreaMapId, data []byte, bkt *stor
 	}
 
 	return wc.Close()
-}
-
-type LoadAreaMapsInJob struct {
-	AreaId sotah.AreaMapId
-	Data   []byte
-}
-
-type LoadAreaMapsOutJob struct {
-	AreaId sotah.AreaMapId
-	Err    error
-}
-
-func (job LoadAreaMapsOutJob) ToLogrusFields() logrus.Fields {
-	return logrus.Fields{
-		"error":   job.Err.Error(),
-		"area-id": job.AreaId,
-	}
-}
-
-func (b AreaMapsBase) LoadAreaMaps(
-	in chan LoadAreaMapsInJob,
-	bkt *storage.BucketHandle,
-) chan LoadAreaMapsOutJob {
-	// establishing channels
-	out := make(chan LoadAreaMapsOutJob)
-
-	// spinning up workers for receiving area-map bytes and persisting it
-	worker := func() {
-		for job := range in {
-			if err := b.WriteObject(job.AreaId, job.Data, bkt); err != nil {
-				out <- LoadAreaMapsOutJob{
-					AreaId: job.AreaId,
-					Err:    err,
-				}
-
-				continue
-			}
-
-			out <- LoadAreaMapsOutJob{
-				AreaId: job.AreaId,
-				Err:    nil,
-			}
-		}
-	}
-	postWork := func() {
-		close(out)
-	}
-	util.Work(2, worker, postWork)
-
-	return out
 }
