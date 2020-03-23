f38f18f4db2c09e4c315798c15f5e29cee57bbdb
Adrian Parker <ihsw.aparker@gmail.com>
2020-03-22 17:40:00 -0700

    Start connected-realm functionality.

---
+++ pkg/blizzard/connectedrealm_index.go
@@ -0,0 +1,66 @@
+package blizzard
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
+)
+
+const connectedRealmIndexURLFormat = "https://%s/data/wow/connected-realm/index"
+
+func DefaultConnectedRealmIndexURL(regionHostname string) (string, error) {
+	return fmt.Sprintf(connectedRealmIndexURLFormat, regionHostname), nil
+}
+
+type GetConnectedRealmIndexURLFunc func(string) (string, error)
+
+type ConnectedRealmIndexResponse struct {
+	Links           SelfReference   `json:"_links"`
+	ConnectedRealms []HrefReference `json:"connected_realms"`
+}
+
+func NewConnectedRealmIndexFromHTTP(uri string) (ConnectedRealmIndexResponse, ResponseMeta, error) {
+	resp, err := Download(uri)
+	if err != nil {
+		logging.WithFields(logrus.Fields{
+			"error": err.Error(),
+			"uri":   uri,
+		}).Error("failed to download connected-realm-index")
+
+		return ConnectedRealmIndexResponse{}, resp, err
+	}
+
+	if resp.Status != http.StatusOK {
+		logging.WithFields(logrus.Fields{
+			"status": resp.Status,
+			"uri":    uri,
+		}).Error("resp from connected-realm-index was not 200")
+
+		return ConnectedRealmIndexResponse{}, resp, errors.New("status was not 200")
+	}
+
+	crIndex, err := NewConnectedRealmIndexResponse(resp.Body)
+	if err != nil {
+		logging.WithFields(logrus.Fields{
+			"error": err.Error(),
+			"uri":   uri,
+		}).Error("failed to parse connected-realm-index response")
+
+		return ConnectedRealmIndexResponse{}, resp, err
+	}
+
+	return crIndex, resp, nil
+}
+
+func NewConnectedRealmIndexResponse(body []byte) (ConnectedRealmIndexResponse, error) {
+	crIndex := &ConnectedRealmIndexResponse{}
+	if err := json.Unmarshal(body, crIndex); err != nil {
+		return ConnectedRealmIndexResponse{}, err
+	}
+
+	return *crIndex, nil
+}
