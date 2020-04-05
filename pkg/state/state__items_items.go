731e94c139280f63e0169a214d66b38b908f011b
Adrian Parker <ihsw.aparker@gmail.com>
2020-04-05 11:54:42 -0700

    Further split for cleanliness.

---
+++ pkg/state/state__items_items.go
@@ -0,0 +1,89 @@
+package state
+
+import (
+	"encoding/base64"
+	"encoding/json"
+
+	nats "github.com/nats-io/go-nats"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/messenger"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/messenger/codes"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/sotah"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/state/subjects"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/util"
+)
+
+func NewItemsRequest(payload []byte) (ItemsRequest, error) {
+	iRequest := &ItemsRequest{}
+	err := json.Unmarshal(payload, &iRequest)
+	if err != nil {
+		return ItemsRequest{}, err
+	}
+
+	return *iRequest, nil
+}
+
+type ItemsRequest struct {
+	ItemIds []blizzardv2.ItemId `json:"itemIds"`
+}
+
+type ItemsResponse struct {
+	Items sotah.ItemsMap `json:"items"`
+}
+
+func (iResponse ItemsResponse) EncodeForMessage() (string, error) {
+	encodedResult, err := json.Marshal(iResponse)
+	if err != nil {
+		return "", err
+	}
+
+	gzippedResult, err := util.GzipEncode(encodedResult)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.StdEncoding.EncodeToString(gzippedResult), nil
+}
+
+func (sta ItemsState) ListenForItems(stop ListenStopChan) error {
+	err := sta.Messenger.Subscribe(string(subjects.Items), stop, func(natsMsg nats.Msg) {
+		m := messenger.NewMessage()
+
+		// resolving the request
+		iRequest, err := NewItemsRequest(natsMsg.Data)
+		if err != nil {
+			m.Err = err.Error()
+			m.Code = codes.MsgJSONParseError
+			sta.Messenger.ReplyTo(natsMsg, m)
+
+			return
+		}
+
+		iMap, err := sta.ItemsDatabase.FindItems(iRequest.ItemIds)
+		if err != nil {
+			m.Err = err.Error()
+			m.Code = codes.GenericError
+			sta.Messenger.ReplyTo(natsMsg, m)
+
+			return
+		}
+
+		iResponse := ItemsResponse{Items: iMap}
+		data, err := iResponse.EncodeForMessage()
+		if err != nil {
+			m.Err = err.Error()
+			m.Code = codes.MsgJSONParseError
+			sta.Messenger.ReplyTo(natsMsg, m)
+
+			return
+		}
+
+		m.Data = data
+		sta.Messenger.ReplyTo(natsMsg, m)
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ pkg/state/state__items_itemsquery.go
@@ -0,0 +1,62 @@
+package state
+
+import (
+	nats "github.com/nats-io/go-nats"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/database"
+	dCodes "source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/database/codes"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/messenger"
+	mCodes "source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/messenger/codes"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/state/subjects"
+)
+
+func (sta ItemsState) ListenForItemsQuery(stop ListenStopChan) error {
+	err := sta.Messenger.Subscribe(string(subjects.ItemsQuery), stop, func(natsMsg nats.Msg) {
+		m := messenger.NewMessage()
+
+		// resolving the request
+		request, err := database.NewQueryItemsRequest(natsMsg.Data)
+		if err != nil {
+			m.Err = err.Error()
+			m.Code = mCodes.MsgJSONParseError
+			sta.Messenger.ReplyTo(natsMsg, m)
+
+			return
+		}
+
+		// querying the items-database
+		resp, respCode, err := sta.ItemsDatabase.QueryItems(request)
+		if err != nil {
+			m.Err = err.Error()
+			m.Code = DatabaseCodeToMessengerCode(respCode)
+			sta.Messenger.ReplyTo(natsMsg, m)
+
+			return
+		}
+		if respCode != dCodes.Ok {
+			m.Err = "response code was not ok but error was nil"
+			m.Code = DatabaseCodeToMessengerCode(respCode)
+			sta.Messenger.ReplyTo(natsMsg, m)
+
+			return
+		}
+
+		// marshalling for messenger
+		encodedMessage, err := resp.EncodeForDelivery()
+		if err != nil {
+			m.Err = err.Error()
+			m.Code = mCodes.GenericError
+			sta.Messenger.ReplyTo(natsMsg, m)
+
+			return
+		}
+
+		// dumping it out
+		m.Data = string(encodedMessage)
+		sta.Messenger.ReplyTo(natsMsg, m)
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
