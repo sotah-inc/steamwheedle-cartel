cabf09af2ad6b3ab6fb99649d8e7b916855d9cfc
Adrian Parker <ihsw.aparker@gmail.com>
2020-04-25 11:09:50 -0700

    Start pricelist-histories db functionality.

---
+++ pkg/database/database_pricelisthistories.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"encoding/binary"
+	"fmt"
+
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/sotah"
+)
+
+// keying
+func pricelistHistoryKeyName() []byte {
+	key := make([]byte, 8)
+	binary.LittleEndian.PutUint64(key, 1)
+
+	return key
+}
+
+// bucketing
+func pricelistHistoryBucketName(ID blizzardv2.ItemId) []byte {
+	return []byte(fmt.Sprintf("item-prices/%d", ID))
+}
+
+// db
+func pricelistHistoryDatabaseFilePath(
+	dirPath string,
+	tuple blizzardv2.RegionConnectedRealmTuple,
+	targetTimestamp sotah.UnixTimestamp,
+) string {
+	return fmt.Sprintf(
+		"%s/pricelist-histories/%s/%d/%d.db",
+		dirPath,
+		tuple.RegionName,
+		tuple.ConnectedRealmId,
+		targetTimestamp,
+	)
+}
