32d2c96890324235e3d545981650b42f305fe3be
Adrian Parker <ihsw.aparker@gmail.com>
2020-04-28 15:59:54 -0700

    Fill in func for gathering encoded pricelist-history.

---
+++ pkg/lake/disk/client_auctions_getencodedpricelisthistory.go
@@ -0,0 +1,106 @@
+package disk
+
+import (
+	"github.com/sirupsen/logrus"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2"
+	BaseLake "source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/lake/base"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/sotah"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/util"
+)
+
+func (client Client) GetEncodedPricelistHistoryByTuple(
+	tuple blizzardv2.RegionConnectedRealmTuple,
+) (map[blizzardv2.ItemId][]byte, error) {
+	cachedAuctionsFilepath, err := client.resolveAuctionsFilepath(tuple)
+	if err != nil {
+		return nil, err
+	}
+
+	gzipEncoded, err := util.ReadFile(cachedAuctionsFilepath)
+	if err != nil {
+		return nil, err
+	}
+
+	maList, err := sotah.NewMiniAuctionListFromGzipped(gzipEncoded)
+	if err != nil {
+		return nil, err
+	}
+
+	out := map[blizzardv2.ItemId][]byte{}
+	for id, prices := range sotah.NewItemPricesFromMiniAuctionList(maList) {
+		out[id], err = prices.EncodeForStorage()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return out, nil
+}
+
+type getEncodedPricelistHistoryByTuplesJob struct {
+	err                     error
+	tuple                   blizzardv2.LoadConnectedRealmTuple
+	encodedPricelistHistory map[blizzardv2.ItemId][]byte
+}
+
+func (job getEncodedPricelistHistoryByTuplesJob) Tuple() blizzardv2.LoadConnectedRealmTuple {
+	return job.tuple
+}
+func (job getEncodedPricelistHistoryByTuplesJob) EncodedPricelistHistory() map[blizzardv2.ItemId][]byte {
+	return job.encodedPricelistHistory
+}
+func (job getEncodedPricelistHistoryByTuplesJob) Err() error { return job.err }
+func (job getEncodedPricelistHistoryByTuplesJob) ToLogrusFields() logrus.Fields {
+	return logrus.Fields{
+		"error":           job.err.Error(),
+		"region":          job.tuple.RegionName,
+		"connected-realm": job.tuple.ConnectedRealmId,
+	}
+}
+
+func (client Client) GetEncodedPricelistHistoryByTuples(
+	tuples blizzardv2.LoadConnectedRealmTuples,
+) chan BaseLake.GetEncodedPricelistHistoryByTuplesJob {
+	// establishing channels
+	out := make(chan BaseLake.GetEncodedPricelistHistoryByTuplesJob)
+	in := make(chan blizzardv2.LoadConnectedRealmTuple)
+
+	// spinning up the workers for fetching auctions
+	worker := func() {
+		for tuple := range in {
+			gzipEncoded, err := client.GetEncodedPricelistHistoryByTuple(tuple.RegionConnectedRealmTuple)
+			if err != nil {
+				out <- getEncodedPricelistHistoryByTuplesJob{
+					err:                     err,
+					tuple:                   tuple,
+					encodedPricelistHistory: nil,
+				}
+
+				continue
+			}
+
+			out <- getEncodedPricelistHistoryByTuplesJob{err, tuple, gzipEncoded}
+		}
+	}
+	postWork := func() {
+		close(out)
+	}
+	util.Work(8, worker, postWork)
+
+	// queueing up the tuples
+	go func() {
+		for _, tuple := range tuples {
+			logging.WithFields(logrus.Fields{
+				"region":          tuple.RegionName,
+				"connected-realm": tuple.ConnectedRealmId,
+			}).Debug("queueing up tuple for fetching")
+
+			in <- tuple
+		}
+
+		close(in)
+	}()
+
+	return out
+}
