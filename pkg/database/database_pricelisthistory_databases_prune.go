39b6def7b5f5350b948f1e25851964f4b53ec163
Adrian Parker <ihsw.aparker@gmail.com>
2020-04-28 15:36:41 -0700

    Add func for pruning.

---
+++ pkg/database/database_pricelisthistory_databases_prune.go
@@ -0,0 +1,60 @@
+package database
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/sotah"
+)
+
+func (phdBases *PricelistHistoryDatabases) pruneDatabases(retentionLimitTimestamp sotah.UnixTimestamp) error {
+	logging.WithField("limit", retentionLimitTimestamp).Info("checking for databases to prune")
+
+	for rName, realmDatabases := range phdBases.Databases {
+		for rSlug, databaseShards := range realmDatabases {
+			for unixTimestamp, phdBase := range databaseShards.After(retentionLimitTimestamp) {
+				logging.WithFields(logrus.Fields{
+					"region":             rName,
+					"realm":              rSlug,
+					"database-timestamp": unixTimestamp,
+				}).Debug("removing database from shard map")
+				delete(phdBases.Databases[rName][rSlug], unixTimestamp)
+
+				dbPath := phdBase.db.Path()
+
+				logging.WithFields(logrus.Fields{
+					"region":             rName,
+					"realm":              rSlug,
+					"database-timestamp": unixTimestamp,
+				}).Debug("closing database")
+				if err := phdBase.db.Close(); err != nil {
+					logging.WithFields(logrus.Fields{
+						"region":   rName,
+						"realm":    rSlug,
+						"database": dbPath,
+					}).Error("failed to close database")
+
+					return err
+				}
+
+				logging.WithFields(logrus.Fields{
+					"region":   rName,
+					"realm":    rSlug,
+					"filepath": dbPath,
+				}).Debug("deleting database file")
+				if err := os.Remove(dbPath); err != nil {
+					logging.WithFields(logrus.Fields{
+						"region":   rName,
+						"realm":    rSlug,
+						"database": dbPath,
+					}).Error("failed to remove database file")
+
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
