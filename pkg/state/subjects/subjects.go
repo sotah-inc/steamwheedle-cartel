22cfc87dbe5ca61b16ba3aa17e89a46647855e39
Adrian Parker <ihsw.aparker@gmail.com>
2020-05-01 13:15:31 -0700

    Remove unused.

--- pkg/state/subjects/subjects.go
+++ pkg/state/subjects/subjects.go
@@ -38,31 +38,3 @@ const (
 	PricelistHistoryIntake Subject = "pricelistHistoryIntake"
 	PriceListHistory       Subject = "priceListHistory"
 )
-
-const (
-	GenericTestErrors          Subject = "genericTestErrors"
-	AppMetrics                 Subject = "appMetrics"
-	PricelistHistoriesIntakeV2 Subject = "pricelistHistoriesIntakeV2"
-	RealmModificationDates     Subject = "realmModificationDates"
-)
-
-const (
-	ReceiveRealms                     Subject = "receiveRealms"
-	ReceiveComputedLiveAuctions       Subject = "receiveComputedLiveAuctions"
-	ReceiveComputedPricelistHistories Subject = "receiveComputedPricelistHistories"
-
-	FilterInItemsToSync Subject = "filterInItemsToSync"
-	ReceiveSyncedItems  Subject = "receiveSyncedItems"
-
-	SyncPubsubTopicsMonitor Subject = "syncPubsubTopicsMonitor"
-)
-
-const (
-	CallDownloadAllAuctions          Subject = "callDownloadAllAuctions"
-	CallCleanupAllManifests          Subject = "callCleanupAllManifests"
-	CallCleanupAllAuctions           Subject = "callCleanupAllAuctions"
-	CallComputeAllLiveAuctions       Subject = "callComputeAllLiveAuctions"
-	CallSyncAllItems                 Subject = "callSyncAllItems"
-	CallComputeAllPricelistHistories Subject = "callComputeAllPricelistHistories"
-	CallCleanupAllPricelistHistories Subject = "callCleanupAllPricelistHistories"
-)
