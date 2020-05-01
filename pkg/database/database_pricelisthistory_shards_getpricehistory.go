bfca48290f630c317ba13662eace62b8ff027310
Adrian Parker <ihsw.aparker@gmail.com>
2020-05-01 07:55:58 -0700

    Make querying shards parallel and faster.

--- pkg/database/database_pricelisthistory_shards_getpricehistory.go
+++ pkg/database/database_pricelisthistory_shards_getpricehistory.go
@@ -1,24 +1,75 @@
 package database
 
 import (
+	"github.com/sirupsen/logrus"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/sotah"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/util"
 )
+
+type GetPriceHistoryJob struct {
+	Err          error
+	PriceHistory sotah.PriceHistory
+}
+
+func (job GetPriceHistoryJob) ToLogrusFields(id blizzardv2.ItemId) logrus.Fields {
+	return logrus.Fields{
+		"error": job.Err.Error(),
+		"item":  id,
+	}
+}
 
 func (shards PricelistHistoryDatabaseShards) GetPriceHistory(
 	id blizzardv2.ItemId,
 	lowerBounds sotah.UnixTimestamp,
 	upperBounds sotah.UnixTimestamp,
 ) (sotah.PriceHistory, error) {
-	pHistory := sotah.PriceHistory{}
+	// establish channels
+	in := make(chan PricelistHistoryDatabase)
+	out := make(chan GetPriceHistoryJob)
 
-	for _, phdBase := range shards {
-		receivedHistory, err := phdBase.getItemPriceHistory(id)
-		if err != nil {
-			return sotah.PriceHistory{}, err
+	// spinning up workers for querying price-histories
+	worker := func() {
+		for phdBase := range in {
+			receivedHistory, err := phdBase.getItemPriceHistory(id)
+			if err != nil {
+				out <- GetPriceHistoryJob{
+					Err:          err,
+					PriceHistory: nil,
+				}
+
+				continue
+			}
+
+			out <- GetPriceHistoryJob{
+				Err:          nil,
+				PriceHistory: receivedHistory,
+			}
+		}
+	}
+	postWork := func() {
+		close(out)
+	}
+	util.Work(4, worker, postWork)
+
+	go func() {
+		for _, phdBase := range shards {
+			in <- phdBase
 		}
 
-		pHistory = pHistory.Merge(receivedHistory.Between(lowerBounds, upperBounds))
+		close(in)
+	}()
+
+	pHistory := sotah.PriceHistory{}
+	for job := range out {
+		if job.Err != nil {
+			logging.WithFields(job.ToLogrusFields(id)).Error("failed to fetch pricelist-history for item")
+
+			return sotah.PriceHistory{}, job.Err
+		}
+
+		pHistory = pHistory.Merge(job.PriceHistory.Between(lowerBounds, upperBounds))
 	}
 
 	return pHistory, nil
