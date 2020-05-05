5e32eaa80f99df98012ab623016b6979b70bf9fa
Adrian Parker <ihsw.aparker@gmail.com>
2020-05-05 09:43:50 -0700

    Update interface definition.

--- pkg/collector/base/client.go
+++ pkg/collector/base/client.go
@@ -1,9 +1,5 @@
 package base
 
-import (
-	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2"
-)
-
 type Client interface {
-	Collect() (blizzardv2.ItemIds, error)
+	Collect() error
 }
