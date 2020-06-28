a5051025f5ed8a76561a787fb62768a185228ff8
Adrian Parker <ihsw.aparker@gmail.com>
2020-06-28 13:03:39 -0700

    Add remaining inventory-types.

--- pkg/blizzardv2/inventorytype/inventorytype.go
+++ pkg/blizzardv2/inventorytype/inventorytype.go
@@ -5,16 +5,23 @@ type InventoryType string
 const (
 	NonEquip InventoryType = "NON_EQUIP"
 	Bag      InventoryType = "BAG"
+	Tabard   InventoryType = "TABARD"
 
 	Ranged      InventoryType = "RANGED"
 	RangedRight InventoryType = "RANGEDRIGHT"
+	Ammo        InventoryType = "AMMO"
 	TwoWeapon   InventoryType = "TWOWEAPON"
 	Weapon      InventoryType = "WEAPON"
+	Shield      InventoryType = "SHIELD"
+	OffHand                   = "HOLDABLE"
 
 	Head     InventoryType = "HEAD"
 	Neck     InventoryType = "NECK"
 	Shoulder InventoryType = "SHOULDER"
+	Cloak    InventoryType = "CLOAK"
 	Chest    InventoryType = "CHEST"
+	Robe     InventoryType = "ROBE"
+	Shirt    InventoryType = "BODY"
 	Wrist    InventoryType = "WRIST"
 	Hand     InventoryType = "HAND"
 	Waist    InventoryType = "WAIST"
