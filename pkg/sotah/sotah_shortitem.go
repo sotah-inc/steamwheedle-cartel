b7516603b64c74620da699e626ce3d5583568949
Adrian Parker <ihsw.aparker@gmail.com>
2020-09-11 13:39:13 -0700

    Add quality to short-item struct.

--- pkg/sotah/sotah_shortitem.go
+++ pkg/sotah/sotah_shortitem.go
@@ -2,6 +2,7 @@ package sotah
 
 import (
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2/itemquality"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2/locale"
 )
 
@@ -27,16 +28,31 @@ func NewShortItem(item Item, locale locale.Locale) (ShortItem, error) {
 		return ShortItem{}, err
 	}
 
+	foundQualityName, err := item.BlizzardMeta.Quality.Name.Find(locale)
+	if err != nil {
+		return ShortItem{}, err
+	}
+
 	return ShortItem{
 		SotahMeta: item.SotahMeta,
 		Id:        item.BlizzardMeta.Id,
 		Name:      foundName,
+		Quality: ShortItemQuality{
+			Type: item.BlizzardMeta.Quality.Type,
+			Name: foundQualityName,
+		},
 	}, nil
+}
+
+type ShortItemQuality struct {
+	Type itemquality.ItemQuality `json:"type"`
+	Name string                  `json:"name"`
 }
 
 type ShortItem struct {
 	SotahMeta ItemMeta `json:"sotah_meta"`
 
-	Id   blizzardv2.ItemId `json:"id"`
-	Name string            `json:"name"`
+	Id      blizzardv2.ItemId `json:"id"`
+	Name    string            `json:"name"`
+	Quality ShortItemQuality  `json:"quality"`
 }
