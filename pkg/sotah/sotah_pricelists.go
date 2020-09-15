d6b1d7e4be49ea34b6e0421b0698aa288911d258
Adrian Parker <ihsw.aparker@gmail.com>
2020-09-15 09:32:29 -0700

    Add logging for debugging.

--- pkg/sotah/sotah_pricelists.go
+++ pkg/sotah/sotah_pricelists.go
@@ -10,7 +10,7 @@ import (
 	"strconv"
 
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2"
-
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/util"
 )
 
@@ -154,6 +154,8 @@ func NewPriceHistoryFromBytes(data []byte) (PriceHistory, error) {
 	if err != nil {
 		return PriceHistory{}, err
 	}
+
+	logging.WithField("data", string(gzipDecoded)).Info("decoding")
 
 	out := PriceHistory{}
 	if err := json.Unmarshal(gzipDecoded, &out); err != nil {
