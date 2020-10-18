8a4750681a436d397bda22b53b121c69a724cb21
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2020-10-18 10:50:38 -0700

    Start professions struct.

---
+++ pkg/blizzardv2/professions_profession.go
@@ -0,0 +1,27 @@
+package blizzardv2
+
+import "source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2/locale"
+
+type ProfessionSkillTierId int
+
+type ProfessionSkillTier struct {
+	Key  HrefReference         `json:"key"`
+	Name locale.Mapping        `json:"name"`
+	Id   ProfessionSkillTierId `json:"id"`
+}
+
+type Profession struct {
+	LinksBase
+	Id          ProfessionId   `json:"id"`
+	Name        locale.Mapping `json:"name"`
+	Description locale.Mapping `json:"description"`
+	Type        struct {
+		Type string         `json:"type"`
+		Name locale.Mapping `json:"name"`
+	} `json:"type"`
+	Media struct {
+		Key HrefReference `json:"key"`
+		Id  ProfessionId  `json:"id"`
+	} `json:"media"`
+	SkillTiers []ProfessionSkillTier `json:"skill_tiers"`
+}
