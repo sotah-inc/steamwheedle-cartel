260577bacb7b9b6c16d1555376dc05ba3236c716
Adrian Parker <ihsw.aparker@gmail.com>
2020-10-26 06:59:42 -0700

    Start refactoring mini-auction-list to filter more clearly.

--- pkg/sotah/sotah_miniauctions_miniauctionlist.go
+++ pkg/sotah/sotah_miniauctions_miniauctionlist.go
@@ -63,6 +63,34 @@ func (maList MiniAuctionList) Sort(kind sortkinds.SortKind, direction sortdirect
 	return newMiniAuctionSorter().sort(kind, direction, maList)
 }
 
+type MiniAuctionListFilterCriteria struct {
+	ItemIds []blizzardv2.ItemId
+	PetIds  []blizzardv2.PetId
+}
+
+func (c MiniAuctionListFilterCriteria) IsEmpty() bool {
+	return len(c.ItemIds) == 0 && len(c.PetIds) == 0
+}
+
+type MiniAuctionFlaggedList []MiniAuctionFlagged
+
+func NewMiniAuctionFlaggedList(input MiniAuctionList) MiniAuctionFlaggedList {
+	out := make(MiniAuctionFlaggedList, len(input))
+	for i, auc := range input {
+		out[i] = MiniAuctionFlagged{
+			auc:     auc,
+			flagged: false,
+		}
+	}
+
+	return out
+}
+
+type MiniAuctionFlagged struct {
+	auc     miniAuction
+	flagged bool
+}
+
 func (maList MiniAuctionList) FilterByItemIds(itemIds []blizzardv2.ItemId) MiniAuctionList {
 	out := MiniAuctionList{}
 	for _, ma := range maList {
