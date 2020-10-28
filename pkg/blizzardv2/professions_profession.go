a6a2699bf7b77d7f6179c5119c3b35821a062530
Adrian Parker <ihsw.aparker@gmail.com>
2020-10-27 18:01:56 -0700

    Fill in profession constructor.

--- pkg/blizzardv2/professions_profession.go
+++ pkg/blizzardv2/professions_profession.go
@@ -1,6 +1,14 @@
 package blizzardv2
 
-import "source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2/locale"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2/locale"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
+)
 
 type ProfessionSkillTierId int
 
@@ -12,7 +20,7 @@ type ProfessionSkillTier struct {
 
 type ProfessionId int
 
-type Profession struct {
+type ProfessionResponse struct {
 	LinksBase
 	Id          ProfessionId   `json:"id"`
 	Name        locale.Mapping `json:"name"`
@@ -26,4 +34,46 @@ type Profession struct {
 		Id  ProfessionId  `json:"id"`
 	} `json:"media"`
 	SkillTiers []ProfessionSkillTier `json:"skill_tiers"`
+}
+
+func NewProfessionResponseFromHTTP(uri string) (ProfessionResponse, ResponseMeta, error) {
+	resp, err := Download(DownloadOptions{Uri: uri})
+	if err != nil {
+		logging.WithFields(logrus.Fields{
+			"error": err.Error(),
+			"uri":   ClearAccessToken(uri),
+		}).Error("failed to download profession")
+
+		return ProfessionResponse{}, resp, err
+	}
+
+	if resp.Status != http.StatusOK {
+		logging.WithFields(logrus.Fields{
+			"status": resp.Status,
+			"uri":    ClearAccessToken(uri),
+		}).Error("resp from profession was not 200")
+
+		return ProfessionResponse{}, resp, errors.New("status was not 200")
+	}
+
+	profession, err := NewProfessionResponse(resp.Body)
+	if err != nil {
+		logging.WithFields(logrus.Fields{
+			"error": err.Error(),
+			"uri":   ClearAccessToken(uri),
+		}).Error("failed to parse profession response")
+
+		return ProfessionResponse{}, resp, err
+	}
+
+	return profession, resp, nil
+}
+
+func NewProfessionResponse(body []byte) (ProfessionResponse, error) {
+	profession := &ProfessionResponse{}
+	if err := json.Unmarshal(body, profession); err != nil {
+		return ProfessionResponse{}, err
+	}
+
+	return *profession, nil
 }
