85c9b1ac28692661efeb3ec8ce7300cafb09a83a
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2020-10-30 21:21:24 -0700

    Start filling in recipe struct.

--- pkg/blizzardv2/recipes_recipe.go
+++ pkg/blizzardv2/recipes_recipe.go
@@ -1,3 +1,11 @@
 package blizzardv2
 
+import "source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2/locale"
+
 type RecipeId int
+
+type Recipe struct {
+	LinksBase
+	Id   RecipeId       `json:"id"`
+	Name locale.Mapping `json:"name"`
+}
