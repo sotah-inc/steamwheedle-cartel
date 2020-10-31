f88d95ec4802e3489ef37244686e0c22616ea942
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2020-10-31 16:22:16 -0700

    Fill in profession skill-tier fetching.

--- pkg/blizzardv2/professions_professionskilltier.go
+++ pkg/blizzardv2/professions_professionskilltier.go
@@ -1,6 +1,29 @@
 package blizzardv2
 
-import "source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2/locale"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
+
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2/locale"
+)
+
+const professionSkillTierURLFormat = "https://%s/data/wow/profession/%d/skill-tier/%d?namespace=static-%s"
+
+func DefaultProfessionSkillTierURL(
+	regionHostname string,
+	professionId ProfessionId,
+	skillTierId ProfessionSkillTierId,
+	regionName RegionName,
+) string {
+	return fmt.Sprintf(professionSkillTierURLFormat, regionHostname, professionId, skillTierId, regionName)
+}
+
+type GetProfessionSkillTierURLFunc func(string) string
 
 type ProfessionSkillTierId int
 
@@ -15,11 +38,53 @@ type ProfessionSkillTierCategory struct {
 	Recipes []ProfessionSkillTierCategoryRecipe `json:"recipes"`
 }
 
-type ProfessionSkillTier struct {
+type ProfessionSkillTierResponse struct {
 	LinksBase
 	Id                ProfessionSkillTierId         `json:"id"`
 	Name              locale.Mapping                `json:"name"`
 	MinimumSkillLevel int                           `json:"minimum_skill_level"`
 	MaximumSkillLevel int                           `json:"maximum_skill_level"`
 	Categories        []ProfessionSkillTierCategory `json:"categories"`
+}
+
+func NewProfessionSkillTierResponse(body []byte) (ProfessionSkillTierResponse, error) {
+	psTier := &ProfessionSkillTierResponse{}
+	if err := json.Unmarshal(body, psTier); err != nil {
+		return ProfessionSkillTierResponse{}, err
+	}
+
+	return *psTier, nil
+}
+
+func NewProfessionSkillTierResponseFromHTTP(uri string) (ProfessionSkillTierResponse, ResponseMeta, error) {
+	resp, err := Download(DownloadOptions{Uri: uri})
+	if err != nil {
+		logging.WithFields(logrus.Fields{
+			"error": err.Error(),
+			"uri":   ClearAccessToken(uri),
+		}).Error("failed to download profession skill-tier")
+
+		return ProfessionSkillTierResponse{}, resp, err
+	}
+
+	if resp.Status != http.StatusOK {
+		logging.WithFields(logrus.Fields{
+			"status": resp.Status,
+			"uri":    ClearAccessToken(uri),
+		}).Error("resp from profession skill-tier was not 200")
+
+		return ProfessionSkillTierResponse{}, resp, errors.New("status was not 200")
+	}
+
+	psTier, err := NewProfessionSkillTierResponse(resp.Body)
+	if err != nil {
+		logging.WithFields(logrus.Fields{
+			"error": err.Error(),
+			"uri":   ClearAccessToken(uri),
+		}).Error("failed to parse profession skill-tier response")
+
+		return ProfessionSkillTierResponse{}, resp, err
+	}
+
+	return psTier, resp, nil
 }
