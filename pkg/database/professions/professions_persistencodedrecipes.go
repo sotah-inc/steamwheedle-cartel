09d55b44b9c014b6f56d4565a1759cbd7757e5e6
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2020-11-04 19:13:08 -0700

    Add func for persisting encoded recipes.

---
+++ pkg/database/professions/professions_persistencodedrecipes.go
@@ -0,0 +1,40 @@
+package professions
+
+import (
+	"github.com/boltdb/bolt"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
+)
+
+type PersistEncodedRecipesInJob struct {
+	RecipeId      blizzardv2.RecipeId
+	EncodedRecipe []byte
+}
+
+func (pdBase Database) PersistEncodedRecipes(in chan PersistEncodedRecipesInJob) (int, error) {
+	logging.Info("persisting encoded recipes")
+
+	totalPersisted := 0
+
+	err := pdBase.db.Batch(func(tx *bolt.Tx) error {
+		rBucket, err := tx.CreateBucketIfNotExists(recipesBucketName())
+		if err != nil {
+			return err
+		}
+
+		for job := range in {
+			if err := rBucket.Put(recipeKeyName(job.RecipeId), job.EncodedRecipe); err != nil {
+				return err
+			}
+
+			totalPersisted += 1
+		}
+
+		return nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	return totalPersisted, nil
+}
