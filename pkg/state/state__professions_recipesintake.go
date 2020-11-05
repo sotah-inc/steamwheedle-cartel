71caa9ba82c829364297eb198423163829a51cd3
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2020-11-05 16:36:14 -0700

    Base64 encode recipes-intake response.

--- pkg/state/state__professions_recipesintake.go
+++ pkg/state/state__professions_recipesintake.go
@@ -1,30 +1,44 @@
 package state
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"time"
-
-	ProfessionsDatabase "source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/database/professions"
 
 	"github.com/nats-io/nats.go"
 	"github.com/sirupsen/logrus"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2"
+	ProfessionsDatabase "source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/database/professions"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/messenger"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/messenger/codes"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/state/subjects"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/util"
 )
 
 func (sta ProfessionsState) ListenForRecipesIntake(stop ListenStopChan) error {
 	err := sta.Messenger.Subscribe(string(subjects.RecipesIntake), stop, func(natsMsg nats.Msg) {
 		m := messenger.NewMessage()
 
-		if err := sta.RecipesIntake(); err != nil {
+		resp, err := sta.RecipesIntake()
+		if err != nil {
 			m.Err = err.Error()
 			m.Code = codes.GenericError
 			sta.Messenger.ReplyTo(natsMsg, m)
 
 			return
 		}
+
+		encodedResp, err := resp.EncodeForDelivery()
+		if err != nil {
+			m.Err = err.Error()
+			m.Code = codes.GenericError
+			sta.Messenger.ReplyTo(natsMsg, m)
+
+			return
+		}
+
+		m.Data = encodedResp
 
 		sta.Messenger.ReplyTo(natsMsg, m)
 	})
@@ -35,7 +49,44 @@ func (sta ProfessionsState) ListenForRecipesIntake(stop ListenStopChan) error {
 	return nil
 }
 
-func (sta ProfessionsState) RecipesIntake() error {
+func NewRecipesIntakeResponse(base64Encoded string) (RecipesIntakeResponse, error) {
+	gzipEncoded, err := base64.StdEncoding.DecodeString(base64Encoded)
+	if err != nil {
+		return RecipesIntakeResponse{}, err
+	}
+
+	jsonEncoded, err := util.GzipDecode(gzipEncoded)
+	if err != nil {
+		return RecipesIntakeResponse{}, err
+	}
+
+	out := RecipesIntakeResponse{}
+	if err := json.Unmarshal(jsonEncoded, &out); err != nil {
+		return RecipesIntakeResponse{}, err
+	}
+
+	return out, nil
+}
+
+type RecipesIntakeResponse struct {
+	RecipeItemIds []blizzardv2.ItemId `json:"recipe_item_ids"`
+}
+
+func (resp RecipesIntakeResponse) EncodeForDelivery() (string, error) {
+	jsonEncoded, err := json.Marshal(resp)
+	if err != nil {
+		return "", err
+	}
+
+	gzipEncoded, err := util.GzipEncode(jsonEncoded)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.StdEncoding.EncodeToString(gzipEncoded), nil
+}
+
+func (sta ProfessionsState) RecipesIntake() (RecipesIntakeResponse, error) {
 	startTime := time.Now()
 
 	// gathering profession recipe-ids
@@ -46,7 +97,7 @@ func (sta ProfessionsState) RecipesIntake() error {
 			err.Error(),
 		).Error("failed to get profession recipe-ids")
 
-		return err
+		return RecipesIntakeResponse{}, err
 	}
 
 	// gathering current recipe-ids
@@ -54,7 +105,7 @@ func (sta ProfessionsState) RecipesIntake() error {
 	if err != nil {
 		logging.WithField("error", err.Error()).Error("failed to get current recipe-ids")
 
-		return err
+		return RecipesIntakeResponse{}, err
 	}
 
 	// resolving recipe-ids to fetch
@@ -105,7 +156,15 @@ func (sta ProfessionsState) RecipesIntake() error {
 	if err != nil {
 		logging.WithField("error", err.Error()).Error("failed to persist recipe")
 
-		return err
+		return RecipesIntakeResponse{}, err
+	}
+
+	// gathering recipe item-ids
+	recipeItemIds, err := sta.ProfessionsDatabase.GetRecipeItemIds()
+	if err != nil {
+		logging.WithField("error", err.Error()).Error("failed to gather recipe item-ids")
+
+		return RecipesIntakeResponse{}, err
 	}
 
 	logging.WithFields(logrus.Fields{
@@ -113,5 +172,9 @@ func (sta ProfessionsState) RecipesIntake() error {
 		"duration-in-ms": time.Since(startTime).Milliseconds(),
 	}).Info("total persisted in recipes-intake")
 
-	return nil
+	resp := RecipesIntakeResponse{
+		RecipeItemIds: recipeItemIds,
+	}
+
+	return resp, nil
 }
