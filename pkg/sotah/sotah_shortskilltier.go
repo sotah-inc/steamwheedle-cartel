5ba7ceb830f02e3d5247adccb20df42f46188ec1
Adrian Parker <ihsw.aparker@gmail.com>
2020-11-07 08:05:27 -0700

    Fill in short-skill-tier struct.

--- pkg/sotah/sotah_shortskilltier.go
+++ pkg/sotah/sotah_shortskilltier.go
@@ -5,12 +5,58 @@ import (
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2/locale"
 )
 
+func NewShortSkillTierCategoryRecipes(
+	recipes []blizzardv2.SkillTierCategoryRecipe,
+	providedLocale locale.Locale,
+) []ShortSkillTierCategoryRecipe {
+	out := make([]ShortSkillTierCategoryRecipe, len(recipes))
+	for i, recipe := range recipes {
+		out[i] = ShortSkillTierCategoryRecipe{
+			Id:   recipe.Id,
+			Name: recipe.Name.FindOr(providedLocale, ""),
+		}
+	}
+
+	return out
+}
+
+type ShortSkillTierCategoryRecipe struct {
+	Id   blizzardv2.RecipeId `json:"id"`
+	Name string              `json:"name"`
+}
+
+func NewShortSkillTierCategories(
+	categories []blizzardv2.SkillTierCategory,
+	providedLocale locale.Locale,
+) []ShortSkillTierCategory {
+	out := make([]ShortSkillTierCategory, len(categories))
+	for i, category := range categories {
+		out[i] = ShortSkillTierCategory{
+			Name:    category.Name.FindOr(providedLocale, ""),
+			Recipes: NewShortSkillTierCategoryRecipes(category.Recipes, providedLocale),
+		}
+	}
+
+	return out
+}
+
+type ShortSkillTierCategory struct {
+	Name    string                         `json:"locale"`
+	Recipes []ShortSkillTierCategoryRecipe `json:"recipes"`
+}
+
 func NewShortSkillTier(skillTier SkillTier, providedLocale locale.Locale) ShortSkillTier {
 	return ShortSkillTier{
-		Id: skillTier.BlizzardMeta.Id,
+		Id:         skillTier.BlizzardMeta.Id,
+		Name:       skillTier.BlizzardMeta.Name.FindOr(providedLocale, ""),
+		Categories: NewShortSkillTierCategories(skillTier.BlizzardMeta.Categories, providedLocale),
 	}
 }
 
 type ShortSkillTier struct {
-	Id blizzardv2.SkillTierId `json:"id"`
+	Id                blizzardv2.SkillTierId   `json:"id"`
+	Name              string                   `json:"name"`
+	MinimumSkillLevel int                      `json:"minimum_skill_level"`
+	MaximumSkillLevel int                      `json:"maximum_skill_level"`
+	Categories        []ShortSkillTierCategory `json:"categories"`
 }
