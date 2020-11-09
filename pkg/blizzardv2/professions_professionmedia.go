c10692ae4748c73efa3c25102a59d60f7ae28dc2
Adrian Parker <ihsw.aparker@gmail.com>
2020-11-08 20:33:14 -0700

    Fill in profession-medias fetching.

---
+++ pkg/blizzardv2/professions_professionmedia.go
@@ -0,0 +1,64 @@
+package blizzardv2
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
+)
+
+type ProfessionMediaResponseAsset struct {
+	Key        string `json:"key"`
+	Value      string `json:"value"`
+	FileDataId int    `json:"file_data_id"`
+}
+
+type ProfessionMediaResponse struct {
+	LinksBase
+	Assets []ProfessionMediaResponseAsset `json:"assets"`
+	Id     ProfessionId                   `json:"id"`
+}
+
+func NewProfessionMediaResponseFromHTTP(uri string) (ProfessionMediaResponse, ResponseMeta, error) {
+	resp, err := Download(DownloadOptions{Uri: uri})
+	if err != nil {
+		logging.WithFields(logrus.Fields{
+			"error": err.Error(),
+			"uri":   ClearAccessToken(uri),
+		}).Error("failed to download profession-media")
+
+		return ProfessionMediaResponse{}, resp, err
+	}
+
+	if resp.Status != http.StatusOK {
+		logging.WithFields(logrus.Fields{
+			"status": resp.Status,
+			"uri":    ClearAccessToken(uri),
+		}).Error("resp from profession-media was not 200")
+
+		return ProfessionMediaResponse{}, resp, errors.New("status was not 200")
+	}
+
+	professionMedia, err := NewProfessionMediaResponse(resp.Body)
+	if err != nil {
+		logging.WithFields(logrus.Fields{
+			"error": err.Error(),
+			"uri":   ClearAccessToken(uri),
+		}).Error("failed to parse profession-media response")
+
+		return ProfessionMediaResponse{}, resp, err
+	}
+
+	return professionMedia, resp, nil
+}
+
+func NewProfessionMediaResponse(body []byte) (ProfessionMediaResponse, error) {
+	professionMedia := &ProfessionMediaResponse{}
+	if err := json.Unmarshal(body, professionMedia); err != nil {
+		return ProfessionMediaResponse{}, err
+	}
+
+	return *professionMedia, nil
+}
---
+++ pkg/blizzardv2/professions_professionmedias.go
@@ -0,0 +1,68 @@
+package blizzardv2
+
+import (
+	"github.com/sirupsen/logrus"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/util"
+)
+
+type GetProfessionMediasInJob struct {
+	ProfessionResponse ProfessionResponse
+}
+
+type GetProfessionMediasOutJob struct {
+	Err                     error
+	ProfessionResponse      ProfessionResponse
+	ProfessionMediaResponse ProfessionMediaResponse
+}
+
+func (job GetProfessionMediasOutJob) ToLogrusFields() logrus.Fields {
+	return logrus.Fields{
+		"error":      job.Err.Error(),
+		"profession": job.ProfessionResponse.Id,
+	}
+}
+
+func GetProfessionsMedias(
+	in chan GetProfessionMediasInJob,
+	getProfessionMediaURL func(string) (string, error),
+) (chan GetProfessionMediasOutJob, error) {
+	// starting up workers for gathering individual professions
+	out := make(chan GetProfessionMediasOutJob)
+	worker := func() {
+		for job := range in {
+			professionMediaUrl, err := getProfessionMediaURL(job.ProfessionResponse.Media.Key.Href)
+			if err != nil {
+				out <- GetProfessionMediasOutJob{
+					Err:                     err,
+					ProfessionResponse:      job.ProfessionResponse,
+					ProfessionMediaResponse: ProfessionMediaResponse{},
+				}
+
+				continue
+			}
+
+			professionMediaResponse, _, err := NewProfessionMediaResponseFromHTTP(professionMediaUrl)
+			if err != nil {
+				out <- GetProfessionMediasOutJob{
+					Err:                     err,
+					ProfessionResponse:      job.ProfessionResponse,
+					ProfessionMediaResponse: ProfessionMediaResponse{},
+				}
+
+				continue
+			}
+
+			out <- GetProfessionMediasOutJob{
+				Err:                     nil,
+				ProfessionResponse:      job.ProfessionResponse,
+				ProfessionMediaResponse: professionMediaResponse,
+			}
+		}
+	}
+	postWork := func() {
+		close(out)
+	}
+	util.Work(4, worker, postWork)
+
+	return out, nil
+}
