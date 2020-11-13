d08579970a55583ee2391ca729ca722c20fc99fc
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2020-11-12 20:47:50 -0700

    Add recipe-media fetcher.

---
+++ pkg/blizzardv2/recipes_recipemedia.go
@@ -0,0 +1,64 @@
+package blizzardv2
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
+)
+
+type RecipeMediaResponseAsset struct {
+	Key        string `json:"key"`
+	Value      string `json:"value"`
+	FileDataId int    `json:"file_data_id"`
+}
+
+type RecipeMediaResponse struct {
+	LinksBase
+	Assets []RecipeMediaResponseAsset `json:"assets"`
+	Id     RecipeId                   `json:"id"`
+}
+
+func NewRecipeMediaResponse(body []byte) (RecipeMediaResponse, error) {
+	rmResp := &RecipeMediaResponse{}
+	if err := json.Unmarshal(body, rmResp); err != nil {
+		return RecipeMediaResponse{}, err
+	}
+
+	return *rmResp, nil
+}
+
+func NewRecipeMediaResponseFromHTTP(uri string) (RecipeMediaResponse, ResponseMeta, error) {
+	resp, err := Download(DownloadOptions{Uri: uri})
+	if err != nil {
+		logging.WithFields(logrus.Fields{
+			"error": err.Error(),
+			"uri":   ClearAccessToken(uri),
+		}).Error("failed to download recipe-media")
+
+		return RecipeMediaResponse{}, resp, err
+	}
+
+	if resp.Status != http.StatusOK {
+		logging.WithFields(logrus.Fields{
+			"status": resp.Status,
+			"uri":    ClearAccessToken(uri),
+		}).Error("resp from recipe-media was not 200")
+
+		return RecipeMediaResponse{}, resp, errors.New("status was not 200")
+	}
+
+	rmResp, err := NewRecipeMediaResponse(resp.Body)
+	if err != nil {
+		logging.WithFields(logrus.Fields{
+			"error": err.Error(),
+			"uri":   ClearAccessToken(uri),
+		}).Error("failed to parse recipe-media response")
+
+		return RecipeMediaResponse{}, resp, err
+	}
+
+	return rmResp, resp, nil
+}
