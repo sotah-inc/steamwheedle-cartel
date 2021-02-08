d832c0458bf1952360fdab5318f66cac810fb4db
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-02-08 11:47:44 -0700

    Add receiver of region-timestamps.

---
+++ pkg/database/regions/regions_receiveregiontimestamps.go
@@ -0,0 +1,44 @@
+package regions
+
+import (
+	"errors"
+
+	"github.com/boltdb/bolt"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/sotah"
+)
+
+func (rBase Database) ReceiveRegionTimestamps(regionTimestamps sotah.RegionTimestamps) error {
+	return rBase.db.Batch(func(tx *bolt.Tx) error {
+		for regionName, connectedRealmTimestamps := range regionTimestamps {
+			bkt, err := tx.CreateBucketIfNotExists(connectedRealmsBucketName(regionName))
+			if err != nil {
+				return err
+			}
+
+			for id, timestamps := range connectedRealmTimestamps {
+				k := connectedRealmsKeyName(id)
+				data := bkt.Get(k)
+				if data == nil {
+					return errors.New("could not find connected-realm by id")
+				}
+
+				realmComposite, err := sotah.NewRealmCompositeFromStorage(data)
+				if err != nil {
+					return err
+				}
+
+				realmComposite.ModificationDates = realmComposite.ModificationDates.Merge(timestamps)
+				encoded, err := realmComposite.EncodeForStorage()
+				if err != nil {
+					return err
+				}
+
+				if err := bkt.Put(k, encoded); err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	})
+}
