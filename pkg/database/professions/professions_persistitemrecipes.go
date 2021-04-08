d0484ae8b69c85c882ea52add53b8c63e84df258
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-04-08 09:59:13 -0700

    Continue to refine logging.

--- pkg/database/professions/professions_persistitemrecipes.go
+++ pkg/database/professions/professions_persistitemrecipes.go
@@ -37,7 +37,7 @@ func (pdBase Database) PersistItemRecipes(providedItemRecipes blizzardv2.ItemRec
 			logging.WithFields(logrus.Fields{
 				"item": itemId,
 				"recipes": recipeIds,
-			}).Info("persisting")
+			}).Info("persisting item-recipes")
 
 			encodedRecipeIds, err := recipeIds.EncodeForStorage()
 			if err != nil {
