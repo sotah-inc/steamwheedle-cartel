072c55023f3e577b35d8a65575402f45d8b1fea6
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-04-10 20:40:40 -0700

    Add logging.

--- pkg/database/professions/professions_getitemnamesmap.go
+++ pkg/database/professions/professions_getitemnamesmap.go
@@ -3,6 +3,7 @@ package professions
 import (
 	"github.com/boltdb/bolt"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2/locale"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/sotah"
 )
 
@@ -15,24 +16,29 @@ func (pdBase Database) GetIdNormalizedNameMap() (sotah.RecipeIdNameMap, error) {
 			return nil
 		}
 
-		err := bkt.ForEach(func(k, v []byte) error {
+		return bkt.ForEach(func(k, v []byte) error {
 			recipeId, err := recipeIdFromNameKeyName(k)
 			if err != nil {
+				logging.WithField(
+					"error",
+					err.Error(),
+				).Error("failed to call recipeIdFromNameKeyName")
+
 				return err
 			}
 
 			out[recipeId], err = locale.NewMapping(v)
 			if err != nil {
+				logging.WithField(
+					"error",
+					err.Error(),
+				).Error("failed to call locale.NewMapping")
+
 				return err
 			}
 
 			return nil
 		})
-		if err != nil {
-			return err
-		}
-
-		return nil
 	})
 	if err != nil {
 		return sotah.RecipeIdNameMap{}, err
