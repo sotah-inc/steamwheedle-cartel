57e44a5602e3549dc8aa6903c55d9afafcf73279
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-04-18 10:13:48 -0700

    Shorten collect sequence for debugging.

--- pkg/collector/disk/client_collect.go
+++ pkg/collector/disk/client_collect.go
@@ -3,7 +3,6 @@ package disk
 import (
 	"time"
 
-	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
 )
 
@@ -11,20 +10,20 @@ func (c Client) Collect() error {
 	startTime := time.Now()
 	logging.Info("calling DiskCollector.Collect()")
 
-	collectAuctionsResults, err := c.collectAuctions()
-	if err != nil {
-		return err
-	}
+	//collectAuctionsResults, err := c.collectAuctions()
+	//if err != nil {
+	//	return err
+	//}
 
-	if err := c.CallLiveAuctionsIntake(
-		collectAuctionsResults.tuples.RegionConnectedRealmTuples(),
-	); err != nil {
-		return err
-	}
+	//if err := c.CallLiveAuctionsIntake(
+	//	collectAuctionsResults.tuples.RegionConnectedRealmTuples(),
+	//); err != nil {
+	//	return err
+	//}
 
-	if err := c.CallItemPricesIntake(collectAuctionsResults.tuples); err != nil {
-		return err
-	}
+	//if err := c.CallItemPricesIntake(collectAuctionsResults.tuples); err != nil {
+	//	return err
+	//}
 
 	if err := c.CallPetsIntake(); err != nil {
 		return err
@@ -43,30 +42,32 @@ func (c Client) Collect() error {
 		return err
 	}
 
-	if err := c.CallRecipePricesIntake(collectAuctionsResults.tuples); err != nil {
-		return err
-	}
+	logging.WithField("recipe-item-ids", recipesItemIds).Info("receive recipe-items")
 
-	if err := c.CallStatsIntake(collectAuctionsResults.tuples); err != nil {
-		return err
-	}
+	//if err := c.CallRecipePricesIntake(collectAuctionsResults.tuples); err != nil {
+	//	return err
+	//}
+
+	//if err := c.CallStatsIntake(collectAuctionsResults.tuples); err != nil {
+	//	return err
+	//}
 
 	if err := c.CallPrunePricelistHistories(); err != nil {
 		return err
 	}
 
 	// resolving next item-ids from auctions and recipes intake
-	nextItemIds := blizzardv2.ItemIdsMap{}
-	for _, id := range collectAuctionsResults.itemIds {
-		nextItemIds[id] = struct{}{}
-	}
-	for _, id := range recipesItemIds {
-		nextItemIds[id] = struct{}{}
-	}
+	//nextItemIds := blizzardv2.ItemIdsMap{}
+	//for _, id := range collectAuctionsResults.itemIds {
+	//	nextItemIds[id] = struct{}{}
+	//}
+	//for _, id := range recipesItemIds {
+	//	nextItemIds[id] = struct{}{}
+	//}
 
-	if err := c.CallItemsIntake(nextItemIds.ToItemIds()); err != nil {
-		return err
-	}
+	//if err := c.CallItemsIntake(nextItemIds.ToItemIds()); err != nil {
+	//	return err
+	//}
 
 	if err := c.CallTokenHistoryIntake(); err != nil {
 		return err
