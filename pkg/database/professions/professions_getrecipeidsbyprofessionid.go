9e41a411197aafa29daa874863f872f65030ca2a
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-04-20 12:25:50 -0700

    Add helpful logging

--- pkg/database/professions/professions_getrecipeidsbyprofessionid.go
+++ pkg/database/professions/professions_getrecipeidsbyprofessionid.go
@@ -2,7 +2,9 @@ package professions
 
 import (
 	"github.com/boltdb/bolt"
+	"github.com/sirupsen/logrus"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/sotah"
 )
 
@@ -11,10 +13,18 @@ func (pdBase Database) GetRecipeIdsByProfessionId(
 ) ([]blizzardv2.RecipeId, error) {
 	var out []blizzardv2.RecipeId
 
+	logging.WithFields(logrus.Fields{
+		"profession": professionId,
+	}).Info("checking for recipe-ids by profession")
+
 	// peeking into the professions database
 	err := pdBase.db.View(func(tx *bolt.Tx) error {
 		stBucket := tx.Bucket(skillTiersBucketName(professionId))
 		if stBucket == nil {
+			logging.WithFields(logrus.Fields{
+				"profession": professionId,
+			}).Info("skill-tiers bucket was empty")
+
 			return nil
 		}
 
@@ -24,8 +34,26 @@ func (pdBase Database) GetRecipeIdsByProfessionId(
 				return err
 			}
 
+			logging.WithFields(logrus.Fields{
+				"profession": professionId,
+				"skill-tier": skillTier,
+			}).Info("checking skill-tier")
+
 			for _, category := range skillTier.BlizzardMeta.Categories {
+				logging.WithFields(logrus.Fields{
+					"profession": professionId,
+					"skill-tier": skillTier,
+					"category":   category,
+				}).Info("checking category")
+
 				for _, categoryRecipe := range category.Recipes {
+					logging.WithFields(logrus.Fields{
+						"profession": professionId,
+						"skill-tier": skillTier,
+						"category":   category,
+						"recipe":     categoryRecipe.Id,
+					}).Info("appending category recipe")
+
 					out = append(out, categoryRecipe.Id)
 				}
 			}
