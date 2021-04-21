e2245dfa3dd3959b94b225398a8072e9a09a85ea
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-04-20 18:38:11 -0700

    Add helpful.

---
+++ bin/update-image-mac.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+ORIGINAL_DIR=`pwd`
+COMMIT_MESSAGE=$1
+git add . \
+  && git commit -m "$COMMIT_MESSAGE" \
+  && git push origin HEAD \
+  && cd extern/steamwheedle-cartel-server \
+  && cd app/ && go get source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git && git status . \
+  && go mod vendor && go mod tidy && git status . \
+  && cd ../ && gcloud builds submit --config ./cloudbuild-gcr.yaml && docker pull gcr.io/sotah-prod/server && git status . \
+  && git add . && git commit -m 'Update to latest.' && git push origin HEAD \
+  && cd $ORIGINAL_DIR && git add extern/ && git commit -m 'Misc.' && git push origin HEAD \
+  && cd ~/sites/venture-co/extern/infra \
+  && docker-compose stop sotah-server-api \
+  && docker-compose rm -fv sotah-server-api \
+  && cd $ORIGINAL_DIR
