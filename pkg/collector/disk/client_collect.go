11a29806310e613217a0d088a10f7811d847dc01
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-04-25 18:38:48 -0700

    Debugging.

--- pkg/collector/disk/client_collect.go
+++ pkg/collector/disk/client_collect.go
@@ -16,46 +16,46 @@ func (c Client) Collect() error {
 		return err
 	}
 
-	if err := c.CallLiveAuctionsIntake(
-		collectAuctionsResults.tuples.RegionConnectedRealmTuples(),
-	); err != nil {
-		return err
-	}
+	//if err := c.CallLiveAuctionsIntake(
+	//	collectAuctionsResults.tuples.RegionConnectedRealmTuples(),
+	//); err != nil {
+	//	return err
+	//}
+	//
+	//if err := c.CallItemPricesIntake(collectAuctionsResults.tuples); err != nil {
+	//	return err
+	//}
+	//
+	//if err := c.CallPetsIntake(); err != nil {
+	//	return err
+	//}
+	//
+	//if err := c.CallProfessionsIntake(); err != nil {
+	//	return err
+	//}
+	//
+	//if err := c.CallSkillTiersIntake(); err != nil {
+	//	return err
+	//}
+	//
+	//recipesItemIds, err := c.CallRecipesIntake()
+	//if err != nil {
+	//	return err
+	//}
 
-	if err := c.CallItemPricesIntake(collectAuctionsResults.tuples); err != nil {
-		return err
-	}
+	//logging.WithField("recipe-item-ids", recipesItemIds).Info("receive recipe-items")
 
-	if err := c.CallPetsIntake(); err != nil {
-		return err
-	}
+	//if err := c.CallRecipePricesIntake(collectAuctionsResults.tuples); err != nil {
+	//	return err
+	//}
 
-	if err := c.CallProfessionsIntake(); err != nil {
-		return err
-	}
+	//if err := c.CallStatsIntake(collectAuctionsResults.tuples); err != nil {
+	//	return err
+	//}
 
-	if err := c.CallSkillTiersIntake(); err != nil {
-		return err
-	}
-
-	recipesItemIds, err := c.CallRecipesIntake()
-	if err != nil {
-		return err
-	}
-
-	logging.WithField("recipe-item-ids", recipesItemIds).Info("receive recipe-items")
-
-	if err := c.CallRecipePricesIntake(collectAuctionsResults.tuples); err != nil {
-		return err
-	}
-
-	if err := c.CallStatsIntake(collectAuctionsResults.tuples); err != nil {
-		return err
-	}
-
-	if err := c.CallPrunePricelistHistories(); err != nil {
-		return err
-	}
+	//if err := c.CallPrunePricelistHistories(); err != nil {
+	//	return err
+	//}
 
 	// resolving next item-ids from auctions and recipes intake
 	nextItemIds := blizzardv2.ItemIds{}.Merge(
@@ -67,13 +67,13 @@ func (c Client) Collect() error {
 		return err
 	}
 
-	if err := c.CallTokenHistoryIntake(); err != nil {
-		return err
-	}
+	//if err := c.CallTokenHistoryIntake(); err != nil {
+	//	return err
+	//}
 
-	if err := c.CallEnchantingRecipeCorrelation(); err != nil {
-		return err
-	}
+	//if err := c.CallEnchantingRecipeCorrelation(); err != nil {
+	//	return err
+	//}
 
 	if err := c.CallRecipeItemCorrelation(); err != nil {
 		return err
