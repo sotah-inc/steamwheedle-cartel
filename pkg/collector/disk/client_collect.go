221eeb8428dc5f272d09499f3277d2566f33f281
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-04-25 20:43:30 -0700

    Restore original workflow.

--- pkg/collector/disk/client_collect.go
+++ pkg/collector/disk/client_collect.go
@@ -16,44 +16,44 @@ func (c Client) Collect() error {
 		return err
 	}
 
-	//if err := c.CallLiveAuctionsIntake(
-	//	collectAuctionsResults.tuples.RegionConnectedRealmTuples(),
-	//); err != nil {
-	//	return err
-	//}
-	//
-	//if err := c.CallItemPricesIntake(collectAuctionsResults.tuples); err != nil {
-	//	return err
-	//}
-	//
-	//if err := c.CallPetsIntake(); err != nil {
-	//	return err
-	//}
-	//
-	//if err := c.CallProfessionsIntake(); err != nil {
-	//	return err
-	//}
-	//
-	//if err := c.CallSkillTiersIntake(); err != nil {
-	//	return err
-	//}
+	if err := c.CallLiveAuctionsIntake(
+		collectAuctionsResults.tuples.RegionConnectedRealmTuples(),
+	); err != nil {
+		return err
+	}
+
+	if err := c.CallItemPricesIntake(collectAuctionsResults.tuples); err != nil {
+		return err
+	}
+
+	if err := c.CallPetsIntake(); err != nil {
+		return err
+	}
+
+	if err := c.CallProfessionsIntake(); err != nil {
+		return err
+	}
+
+	if err := c.CallSkillTiersIntake(); err != nil {
+		return err
+	}
 
 	recipesItemIds, err := c.CallRecipesIntake()
 	if err != nil {
 		return err
 	}
 
-	//if err := c.CallRecipePricesIntake(collectAuctionsResults.tuples); err != nil {
-	//	return err
-	//}
-	//
-	//if err := c.CallStatsIntake(collectAuctionsResults.tuples); err != nil {
-	//	return err
-	//}
-	//
-	//if err := c.CallPrunePricelistHistories(); err != nil {
-	//	return err
-	//}
+	if err := c.CallRecipePricesIntake(collectAuctionsResults.tuples); err != nil {
+		return err
+	}
+
+	if err := c.CallStatsIntake(collectAuctionsResults.tuples); err != nil {
+		return err
+	}
+
+	if err := c.CallPrunePricelistHistories(); err != nil {
+		return err
+	}
 
 	// resolving next item-ids from auctions and recipes intake
 	nextItemIds := blizzardv2.ItemIds{}.Merge(
@@ -65,13 +65,13 @@ func (c Client) Collect() error {
 		return err
 	}
 
-	//if err := c.CallTokenHistoryIntake(); err != nil {
-	//	return err
-	//}
-	//
-	//if err := c.CallEnchantingRecipeCorrelation(); err != nil {
-	//	return err
-	//}
+	if err := c.CallTokenHistoryIntake(); err != nil {
+		return err
+	}
+
+	if err := c.CallEnchantingRecipeCorrelation(); err != nil {
+		return err
+	}
 
 	if err := c.CallRecipeItemCorrelation(); err != nil {
 		return err
