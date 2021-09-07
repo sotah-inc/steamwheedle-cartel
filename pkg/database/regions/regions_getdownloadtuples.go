efd6d2ae434c37dc300610d237a53bb15e50b1ed
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-09-06 19:24:45 -0700

    Add missing.

--- pkg/database/regions/regions_getdownloadtuples.go
+++ pkg/database/regions/regions_getdownloadtuples.go
@@ -3,11 +3,10 @@ package regions
 import (
 	"errors"
 
-	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/sotah/statuskinds"
-
 	"github.com/boltdb/bolt"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/sotah"
+	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/sotah/statuskinds"
 )
 
 func (rBase Database) GetDownloadTuples() ([]blizzardv2.DownloadConnectedRealmTuple, error) {
@@ -16,45 +15,47 @@ func (rBase Database) GetDownloadTuples() ([]blizzardv2.DownloadConnectedRealmTu
 	err := rBase.db.View(func(tx *bolt.Tx) error {
 		baseBucket := tx.Bucket(baseBucketName())
 		if baseBucket == nil {
-			return errors.New("base-bucket does not exist")
+			return nil
 		}
 
-		return baseBucket.ForEach(func(baseBucketKey []byte, regionValue []byte) error {
-			region, err := sotah.NewRegion(regionValue)
-			if err != nil {
-				return err
-			}
+		connectedRealmsBucket := tx.Bucket(connectedRealmsBucketName())
+		if connectedRealmsBucket == nil {
+			return nil
+		}
 
-			connectedRealmsBucket := tx.Bucket(connectedRealmsBucketName())
-			if connectedRealmsBucket == nil {
+		return connectedRealmsBucket.ForEach(
+			func(connectedRealmKey []byte, connectedRealmValue []byte) error {
+				keyTuple, err := tupleFromConnectedRealmKeyName(connectedRealmKey)
+				if err != nil {
+					return err
+				}
+
+				regionValue := baseBucket.Get(baseKeyName(keyTuple.RegionName))
+				if regionValue == nil {
+					return errors.New("could not resolve region")
+				}
+				region, err := sotah.NewRegion(regionValue)
+				if err != nil {
+					return err
+				}
+
+				realmComposite, err := sotah.NewRealmCompositeFromStorage(connectedRealmValue)
+				if err != nil {
+					return err
+				}
+
+				lastModified := realmComposite.StatusTimestamps[statuskinds.Downloaded].Time()
+				out = append(out, blizzardv2.DownloadConnectedRealmTuple{
+					LoadConnectedRealmTuple: blizzardv2.LoadConnectedRealmTuple{
+						RegionVersionConnectedRealmTuple: keyTuple,
+						LastModified:                     lastModified,
+					},
+					RegionHostname: region.Hostname,
+				})
+
 				return nil
-			}
-
-			return connectedRealmsBucket.ForEach(
-				func(connectedRealmKey []byte, connectedRealmValue []byte) error {
-					keyTuple, err := tupleFromConnectedRealmKeyName(connectedRealmKey)
-					if err != nil {
-						return err
-					}
-
-					realmComposite, err := sotah.NewRealmCompositeFromStorage(connectedRealmValue)
-					if err != nil {
-						return err
-					}
-
-					lastModified := realmComposite.StatusTimestamps[statuskinds.Downloaded].Time()
-					out = append(out, blizzardv2.DownloadConnectedRealmTuple{
-						LoadConnectedRealmTuple: blizzardv2.LoadConnectedRealmTuple{
-							RegionVersionConnectedRealmTuple: keyTuple,
-							LastModified:                     lastModified,
-						},
-						RegionHostname: region.Hostname,
-					})
-
-					return nil
-				},
-			)
-		})
+			},
+		)
 	})
 	if err != nil {
 		return []blizzardv2.DownloadConnectedRealmTuple{}, err
