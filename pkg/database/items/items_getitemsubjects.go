c2e98790ac523dc3484906e69a5d79d422138700
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-09-15 11:32:27 -0700

    Fix typo.

--- pkg/database/items/items_getitemsubjects.go
+++ pkg/database/items/items_getitemsubjects.go
@@ -1,8 +1,6 @@
 package items
 
 import (
-	"errors"
-
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/blizzardv2/gameversion"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
@@ -14,16 +12,16 @@ func (idBase Database) GetItemSubjects(
 ) (blizzardv2.ItemSubjectsMap, error) {
 	out := blizzardv2.ItemSubjectsMap{}
 	for job := range idBase.FindItems(version, ids) {
+		if !job.Exists {
+			logging.WithField("id", job.Id).Error("attempted to fetch item not found")
+
+			continue
+		}
+
 		if job.Err != nil {
 			logging.WithFields(job.ToLogrusFields()).Error("failed to find item")
 
 			return blizzardv2.ItemSubjectsMap{}, job.Err
-		}
-
-		if !job.Exists {
-			logging.WithField("id", job.Id).Error("attempted to fetch item not found")
-
-			return blizzardv2.ItemSubjectsMap{}, errors.New("item not found")
 		}
 
 		foundName := job.Item.BlizzardMeta.Name.ResolveDefaultName()
