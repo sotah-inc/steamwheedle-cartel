41569e2894f80603e574608247569113a0db320e
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-10-28 08:10:48 -0700

    Update log line for debugging.

--- bin/update-deps.sh
+++ bin/update-deps.sh
@@ -8,6 +8,6 @@ git add . \
   && cd extern/steamwheedle-cartel-server \
   && cd app/ && go get source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git && git status . \
   && go mod vendor && go mod tidy && git status . \
-  && cd ../ && gcloud builds submit --config ./cloudbuild-gcr.yaml && docker pull gcr.io/sotah-prod/server && git status . \
+  && cd ../ && gcloud builds submit --config ./cloudbuild-gcr.yaml && git status . \
   && git add . && git commit -m 'Update to latest.' && git push origin HEAD \
   && cd $ORIGINAL_DIR && git add extern/ && git commit -m 'Misc.' && git push origin HEAD
