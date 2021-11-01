631f4b75e0a18f3133bd5ada95f7a5a539ba1073
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-11-01 08:45:46 -0700

    Restore full collection.

--- pkg/collector/disk/client_collect.go
+++ pkg/collector/disk/client_collect.go
@@ -3,6 +3,7 @@ package disk
 import (
 	"time"
 
+	"github.com/sirupsen/logrus"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/logging"
 	"source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git/pkg/state"
 )
@@ -22,69 +23,70 @@ func (c Client) Collect() error {
 		return err
 	}
 
-	//if err := c.CallItemPricesIntake(collectAuctionsResults.tuples); err != nil {
-	//	return err
-	//}
+	if err := c.CallItemPricesIntake(collectAuctionsResults.tuples); err != nil {
+		return err
+	}
 
-	//if err := c.CallPetsIntake(); err != nil {
-	//	return err
-	//}
+	if err := c.CallPetsIntake(); err != nil {
+		return err
+	}
 
-	//if err := c.CallProfessionsIntake(); err != nil {
-	//	return err
-	//}
+	if err := c.CallProfessionsIntake(); err != nil {
+		return err
+	}
 
-	//if err := c.CallSkillTiersIntake(); err != nil {
-	//	return err
-	//}
+	if err := c.CallSkillTiersIntake(); err != nil {
+		return err
+	}
 
-	//recipesIntakeResponse, err := c.CallRecipesIntake()
-	//if err != nil {
-	//	return err
-	//}
+	recipesIntakeResponse, err := c.CallRecipesIntake()
+	if err != nil {
+		return err
+	}
 
-	//if err := c.CallRecipePricesIntake(collectAuctionsResults.tuples); err != nil {
-	//	return err
-	//}
+	if err := c.CallRecipePricesIntake(collectAuctionsResults.tuples); err != nil {
+		return err
+	}
 
-	//if err := c.CallStatsIntake(collectAuctionsResults.tuples); err != nil {
-	//	return err
-	//}
+	if err := c.CallStatsIntake(collectAuctionsResults.tuples); err != nil {
+		return err
+	}
 
-	//if err := c.CallPrunePricelistHistories(); err != nil {
-	//	return err
-	//}
+	if err := c.CallPrunePricelistHistories(); err != nil {
+		return err
+	}
 
 	// resolving next item-ids from auctions and recipes intake
-	//logging.WithFields(logrus.Fields{
-	//	"recipe-item-ids": len(recipesIntakeResponse.RecipeItemIds),
-	//}).Info("DID NOT COMBINE RECIPE ITEM-IDS IN INTAKE RESPONSE")
-	//if _, err := c.CallItemsIntake(collectAuctionsResults.versionItems); err != nil {
-	//	return err
-	//}
+	logging.WithFields(logrus.Fields{
+		"recipe-item-ids": len(recipesIntakeResponse.RecipeItemIds),
+	}).Info("DID NOT COMBINE RECIPE ITEM-IDS IN INTAKE RESPONSE")
+	itemIntakeResponse, err := c.CallItemsIntake(collectAuctionsResults.versionItems)
+	if err != nil {
+		return err
+	}
 
-	//if err := c.CallTokenHistoryIntake(); err != nil {
-	//	return err
-	//}
+	if err := c.CallTokenHistoryIntake(); err != nil {
+		return err
+	}
 
-	//if recipesIntakeResponse.TotalPersisted > 0 || itemIntakeResponse.TotalPersisted > 0 {
-	//	recipeItemStartTime := time.Now()
-	//
-	//	logging.Info("starting item-recipes correlation tasks")
-	//
-	//	if err := c.CallEnchantingRecipeCorrelation(); err != nil {
-	//		return err
-	//	}
-	//
-	//	if err := c.CallRecipeItemCorrelation(); err != nil {
-	//		return err
-	//	}
-	//
-	//	logging.WithField(
-	//		"duration-in-ms",
-	//		time.Since(recipeItemStartTime).Milliseconds(),
-	//	).Info("finished calling item-recipes correlation tasks")
-	//}
+	if recipesIntakeResponse.TotalPersisted > 0 || itemIntakeResponse.TotalPersisted > 0 {
+		recipeItemStartTime := time.Now()
+
+		logging.Info("starting item-recipes correlation tasks")
+
+		if err := c.CallEnchantingRecipeCorrelation(); err != nil {
+			return err
+		}
+
+		if err := c.CallRecipeItemCorrelation(); err != nil {
+			return err
+		}
+
+		logging.WithField(
+			"duration-in-ms",
+			time.Since(recipeItemStartTime).Milliseconds(),
+		).Info("finished calling item-recipes correlation tasks")
+	}
 
 	logging.WithField(
 		"duration-in-ms",
