8382eafe585d0ea8d205472059691cf9215aba71
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-11-23 07:43:45 -0700

    Spacing, linting.

--- bin/update-deps.sh
+++ bin/update-deps.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-ORIGINAL_DIR=`pwd`
+ORIGINAL_DIR=$(pwd)
 COMMIT_MESSAGE=$1
 git add ./pkg \
   && git commit -m "$COMMIT_MESSAGE" \
@@ -10,4 +10,4 @@ git add ./pkg \
   && go mod vendor && go mod tidy && git status . \
   && cd ../ && gcloud builds submit --config ./cloudbuild-gcr.yaml && git status . \
   && git add . && git commit -m 'Update to latest.' && git push origin HEAD \
-  && cd $ORIGINAL_DIR && git add extern/ && git commit -m 'Misc.' && git push origin HEAD
+  && cd "$ORIGINAL_DIR" && git add extern/ && git commit -m 'Misc.' && git push origin HEAD
