1c67486c2e1a0cfbede3eacbf6b6f51a80789d13
Adrian Parker <ihsw.aparker.kilabuk@gmail.com>
2021-12-24 20:17:19 -0700

    Fix typo.

--- bin/update-server.sh
+++ bin/update-server.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 cd extern/steamwheedle-cartel-server/app \
-  && cd app/ && go get source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git && git status . \
+  && go get source.developers.google.com/p/sotah-prod/r/steamwheedle-cartel.git && git status . \
   && go mod vendor && go mod tidy && git status .
